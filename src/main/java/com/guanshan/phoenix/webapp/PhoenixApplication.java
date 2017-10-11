5bff5905989754971075e380acf27264198ee917
justin <iheyu22@gmail.com>
2017-10-11 10:08:22 +0800

    配置war包

--- src/main/java/com/guanshan/phoenix/webapp/PhoenixApplication.java
+++ src/main/java/com/guanshan/phoenix/webapp/PhoenixApplication.java
@@ -3,12 +3,19 @@ package com.guanshan.phoenix.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class PhoenixApplication {
+public class PhoenixApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhoenixApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PhoenixApplication.class);
 	}
 }
