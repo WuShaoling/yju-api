1f3c1629d591284d00d83dfcdc67095a7f1685df
justin <iheyu22@gmail.com>
2017-10-16 15:02:23 +0800

    Delete old version code

--- src/main/java/com/guanshan/phoenix/service/TeacherService.java
+++
@@ -1,8 +0,0 @@
-package com.guanshan.phoenix.service;
-
-import com.guanshan.phoenix.webdomain.RespTeacherCourse;
-
-public interface TeacherService {
-
-    RespTeacherCourse getTeacherCourse(int teacherId);
-}
--- src/main/java/com/guanshan/phoenix/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.guanshan.phoenix.service;
-
-
-import com.guanshan.phoenix.dao.entity.User;
-
-public interface UserService {
-
-    User getUserInfo(String username);
-}
