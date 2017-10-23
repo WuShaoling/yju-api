389a5ea1c4e90ea2f86d1deba7936f4af37eb723
wen.shi <elogo1324@live.com>
2017-10-23 17:40:34 +0800

    API course/{courseId}/experiments 去掉/

--- src/main/java/com/guanshan/phoenix/controller/ManagerController.java
+++ src/main/java/com/guanshan/phoenix/controller/ManagerController.java
@@ -232,7 +232,7 @@ public class ManagerController {
     }
 
     @ApiOperation(value = "获取课程实验信息", notes = "")
-    @GetMapping(value = "course/{courseId}/experiments/")
+    @GetMapping(value = "course/{courseId}/experiments")
     public ResponseMessage<ResCourseExperiments> getCourseExperiments(@PathVariable int courseId) throws ApplicationErrorException {
         return new ResponseMessage.Success<>(courseService.getCourseExperiments(courseId));
     }
