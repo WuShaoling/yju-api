d8a7c80c070efbd2cbeef30493c206da465af207
justin <iheyu22@gmail.com>
2017-10-23 17:41:16 +0800

    修正文件服务器路径

--- src/main/java/com/guanshan/phoenix/controller/TeacherController.java
+++ src/main/java/com/guanshan/phoenix/controller/TeacherController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("teacher")
 public class TeacherController {
+
     @Autowired
     private TeacherService teacherService;
 
